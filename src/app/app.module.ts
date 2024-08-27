156455af3ba96675441266bca15f8b28575dead2
rishi-rj-s <rishisanu007@gmail.com>
2024-08-27 12:20:30 +0530

    Created file hierarchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { SharedComponent } from './shared/shared.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { ProfilePicComponent } from './user/profile/profile-pic/profile-pic.component';
+import { UserListComponent } from './admin/user-list/user-list.component';
+import { SearchComponent } from './admin/user-list/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    LoginComponent,
+    SharedComponent,
+    HomeComponent,
+    ProfileComponent,
+    ProfilePicComponent,
+    UserListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user/profile/profile-pic/profile-pic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-pic',
+  templateUrl: './profile-pic.component.html',
+  styleUrl: './profile-pic.component.css'
+})
+export class ProfilePicComponent {
+
+}
